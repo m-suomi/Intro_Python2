72bb04524ed94d9c94d8bfce9e9662da9167e722
mtsuomi <michael.suomi@gmail.com>
2018-02-20 22:52:20 -0700

    update

--- divisors_for.py
+++ divisors_for.py
@@ -1,8 +1,14 @@
 x = int(input('Calculate the divisors of which number? '))
-a = 1
-while a <= x:
-    if x % a == 0:
-        print (a)
-        a += 1
-    else:
-        a += 1
+
+for poss_div in range(1,x+1):
+    if x % poss_div == 0:
+        print (poss_div)
+
+
+#a = 1
+#while a <= x:
+#    if x % a == 0:
+#        print (a)
+#        a += 1
+#    else:
+#        a += 1
--- prime_for.py
+++ prime_for.py
@@ -2,14 +2,17 @@
 
 #use for instead of while loops
 x = int(input('Please input your number: '))
+prime = True
 
 for integer in range(2,x):
     if x % integer == 0:
-        print(str(x) + ' is not a prime number.')
+        prime = False
         break
-        break
-    else:
-        print(str(x) + ' is a prime number.')
+
+if prime:
+    print(str(x) + ' is a prime number.')
+else:
+    print(str(x) + ' is NOT a prime number.')
 
 
 #a = x - 1
